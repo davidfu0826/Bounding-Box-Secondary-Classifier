8649d684d0301a14dd2e742a2c6f962b1313e075
EliteDarkLordDragonSlayer3.14159 <david.bao.fu@gmail.com>
2020-08-29 16:00:30 +0200

    Updated GUI

--- graphical_interface.py
+++ graphical_interface.py
@@ -31,14 +31,109 @@ class MainApplication(tk.Frame):
         
             
         
-        tk.Label(self.frames[0], text='FRAME 1').pack()
-        tk.Label(self.frames[1], text='FRAME 2').pack()
-        tk.Label(self.frames[2], text='FRAME 3').pack()
-        tk.Label(self.frames[3], text='FRAME 4').pack()
+        #tk.Label(self.frames[0], text='FRAME 1').pack()
+        #tk.Label(self.frames[1], text='FRAME 2').pack()
+        #tk.Label(self.frames[2], text='FRAME 3').pack()
+        #tk.Label(self.frames[3], text='FRAME 4').pack()
+        
+        self._load_crop_box_frame(self.frames[0])
+        
         self._raise_frame(self.frames[0])
         
     def _raise_frame(self, frame):
         frame.tkraise()
+        
+    def _load_crop_box_frame(self, parent):
+        # Select output format
+        tk.Label(self.frames[0], text='Select output format:', anchor=tk.W).grid(sticky="W", row=0, column=0)
+        unlabelled = tk.IntVar()
+        tk.Radiobutton(parent, text="Labelled dataset", variable=unlabelled, value=1).grid(row=0, column=1, sticky=tk.W)
+        tk.Radiobutton(parent, text="Unlabelled dataset", variable=unlabelled, value=2).grid(row=1, column=1, sticky=tk.W)
+
+        # Select names file
+        tk.Label(self.frames[0], text='Select names file (.names/.txt):', anchor=tk.W).grid(sticky="W", row=2, column=0)
+        def select_names_file():
+            names_file_path = filedialog.askopenfile().name
+            with open(names_file_path) as f:
+                data = f.readlines()
+                names_string = "".join(data)
+
+            names_file_box.insert(1.0, names_string)
+
+            names_summary_box["text"] = f"Number of unique labels: {len(data)}"
+
+        names_summary_box = tk.Label(parent, width=30, anchor=tk.W)
+        names_summary_box.grid(sticky="W", row=0, column=2, rowspan=2)
+        
+        names_file_box = tk.Text(parent, width=20, height=10)
+        names_file_box.grid(row=2,column=2, rowspan=3)
+        scrollb = tk.Scrollbar(parent, command=names_file_box.yview)
+        names_file_box['yscrollcommand'] = scrollb.set
+        
+        selectNamesFileButton = tk.Button(parent, height=1, width=20, 
+                                        text="Select file", 
+                                        command=lambda: select_names_file())
+        selectNamesFileButton.grid(row=2,column=1)
+
+        
+        # Select image files
+        def get_images():
+            """#After clicking on 'Select images' button
+                #-> Find all images and corresponding labels
+            """
+            img_dir = filedialog.askdirectory()
+            img_paths = glob.glob(os.path.join(img_dir, "*.jpg"))
+            
+            image_paths_box.insert(1.0, "".join([os.path.basename(img_path) + "\n" for img_path in img_paths]))
+            image_summary_box['text'] = f"Number of images: {len(img_paths)}"
+            
+            #if len(img_paths) == len(annot_paths) and len(img_paths) != 0:
+            #    cropButton["state"] = "normal"
+        
+        image_text_label = tk.Label(parent, text="Select image directory:", anchor=tk.W)
+        image_text_label.grid(sticky="W", row=3, column=0)
+        selectImagesButton = tk.Button(parent, height=1, width=20, 
+                                        text="Select directory", 
+                                        command=lambda: get_images())
+        selectImagesButton.grid(row=3, column=1)
+        
+        image_summary_box = tk.Label(parent, width=30, anchor=tk.W)
+        image_summary_box.grid(sticky="W", row=0, column=3, rowspan=2)
+        image_paths_box = tk.Text(parent, width=30, height=10)
+        image_paths_box.grid(row=2,column=3, rowspan=3)
+        img_scrollb = tk.Scrollbar(parent, command=image_paths_box.yview)
+        image_paths_box['yscrollcommand'] = img_scrollb.set
+
+        def get_labels():
+            """#After clicking on 'Select dataset' button
+            #-> Find all images and corresponding labels
+            """
+            labels_path = filedialog.askdirectory()
+            annot_paths = glob.glob(os.path.join(labels_path, "*.txt"))
+            
+            annot_paths_box.insert(1.0, "".join([os.path.basename(annot_path) + "\n" for annot_path in annot_paths]))
+            label_summary_box["text"] = f"Number of labels: {len(annot_paths)}"
+
+        label_dataset_path = tk.Label(parent, text="Select label directory:", anchor=tk.W)
+        label_dataset_path.grid(sticky="W", row=4, column=0)
+        selectLabelsButton = tk.Button(parent, height=1, width=20, 
+                                        text="Select label directory", 
+                                        command=lambda: get_labels())
+        selectLabelsButton.grid(row=4, column=1)
+        
+        label_summary_box = tk.Label(parent, width=30, anchor=tk.W)
+        label_summary_box.grid(sticky="W", row=0, column=4, rowspan=2)
+        
+        annot_paths_box = tk.Text(parent, width=30, height=10)
+        annot_paths_box.grid(row=2,column=4, rowspan=3)
+        annot_scrollb = tk.Scrollbar(parent, command=annot_paths_box.yview)
+        annot_paths_box['yscrollcommand'] = annot_scrollb.set
+
+        cropButton = tk.Button(parent, height=1, width=50, 
+                                text="Crop images", 
+                                command=lambda: crop_images())
+        cropButton.grid(row=5, column=0, columnspan=10)
+        cropButton["state"] = "disabled"
         
         
 
@@ -62,103 +157,4 @@ gui = f1
 button = tk.Button(gui, text="Close", width=25, command=gui.destroy)
 button.pack()
 
-# Select output format
-output_format = 0
-def select_output_format():
-    output_format = listbox.curselection()[0] # 0 for labelled, 1 for unlabelled
-    
-listbox = tk.Listbox(gui, width=50)
-listbox.insert(1, "Output labelled dataset (default)")
-listbox.insert(2, "Output unlabelled dataset (folder with cropped images)")
-listbox.pack()
-select_output_format_button = tk.Button(gui, text="Select output format", width=30, command=lambda: select_output_format())
-select_output_format_button.pack()
-
-# Select names file
-def select_names_file():
-    names_file_path = filedialog.askopenfile().name
-    with open(names_file_path) as f:
-        names_string = "".join(f.readlines())
-        print(names_string)
-    names_file_string.set(names_string)
-names_file_string = tk.StringVar()
-names_file_string.set("")
-names_file_box = tk.Label(gui, textvariable=names_file_string)
-names_file_box.pack()
-selectNamesFileButton = tk.Button(gui, height=1, width=30, 
-                                  text="Select names file (index - name)", 
-                                  command=lambda: select_names_file())
-#command=lambda: retrieve_input() >>> just means do this when i press the button
-selectNamesFileButton.pack()
-
-#menubutton = tk.Menubutton(gui)
-
-def retrieve_input():
-    inputValue=textBox.get("1.0","end-1c")
-    print(inputValue)
-
-textBox = tk.Text(gui, height=2, width=10)
-textBox.pack()
-
-# Crop images
-def get_images():
-"""#After clicking on 'Select images' button
-    #-> Find all images and corresponding labels
-"""
-    dataset_path = filedialog.askdirectory()
-    img_paths = glob.glob(os.path.join(dataset_path, "*.jpg"))
-    annot_paths = [img_path.replace("/images/", "/labels/").replace(".jpg", ".txt") 
-                   for img_path in img_paths]
-    annot_paths = [annot_path for annot_path in annot_paths if os.path.isfile(annot_path)]
-    textBox.set(f"""
-                #Selected directory: '{os.path.basename(dataset_path)}
-                #Number of images found: {len(img_paths)}
-                #Number of labels found: {len(annot_paths)}
-""")
-    if len(img_paths) == len(annot_paths) and len(img_paths) != 0:
-        cropButton["state"] = "normal"
-        
-def get_labels():
-    """#After clicking on 'Select dataset' button
-    #-> Find all images and corresponding labels
-"""
-    labels_path = filedialog.askdirectory()
-    annot_paths = glob.glob(os.path.join(labels_path, "*.txt"))
-    if img_paths is None:
-        if len(annot_paths) != len(img_paths):
-            img_paths = [annot_path.replace("/images/", "/labels/").replace(".jpg", ".txt") 
-                        for annot_path in annot_paths]
-            img_paths = [img_path for img_path in img_paths if os.path.isfile(img_path)]
-    textBox.set(f"""
-                #Selected directory: '{os.path.basename(labels_path)}
-                #Number of images found: {len(img_paths)}
-                #Number of labels found: {len(annot_paths)}
-""")
-    if len(img_paths) == len(annot_paths) and len(img_paths) != 0:
-        cropButton["state"] = "normal"
-    
-    
-textBox = tk.StringVar()
-textBox.set("")
-label_dataset_path = tk.Label(gui, textvariable=textBox)
-label_dataset_path.pack()
-selectImagesButton = tk.Button(gui, height=1, width=10, 
-                                text="Select image directory", 
-                                command=lambda: get_images())
-#command=lambda: retrieve_input() >>> just means do this when i press the button
-selectImagesButton.pack()
-
-selectLabelsButton = tk.Button(gui, height=1, width=10, 
-                                text="Select label directory", 
-                                command=lambda: get_labels())
-#command=lambda: retrieve_input() >>> just means do this when i press the button
-selectLabelsButton.pack()
-
-cropButton = tk.Button(gui, height=1, width=10, 
-                         text="Crop images", 
-                         command=lambda: crop_images())
-cropButton.pack()
-cropButton["state"] = "disabled"
- 
-gui.mainloop()
 """
